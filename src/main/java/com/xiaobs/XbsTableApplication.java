c804ff66b19776122aea55c54c144f56fa0f6da5
hjxGit <4597632@qq.com>
2018-02-12 17:16:49 +0800

    2018-2-12 添加上线倒计时

--- src/main/java/com/xiaobs/XbsTableApplication.java
+++ src/main/java/com/xiaobs/XbsTableApplication.java
@@ -2,10 +2,11 @@ package com.xiaobs;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 @SpringBootApplication
 public class XbsTableApplication {
 
@@ -15,6 +16,6 @@ public class XbsTableApplication {
 
 	@GetMapping(value = "/")
 	public String sayhello(){
-		return "hello world";
+		return "index";
 	}
 }
